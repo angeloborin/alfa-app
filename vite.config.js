49f9aad3ca031165b75f604ebbce9f602494dd13
angeloborin <angeloborin2@gmail.com>
2026-01-19 19:13:58 -0300

    Correção do caminho

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // IMPORTANTE: O 'base' deve ser o nome exato do seu repositório no GitHub
+  // Isso corrige o erro 404 ao procurar os arquivos JS e CSS
+  base: '/alfa-app/', 
 })
