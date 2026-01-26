56a9dac99888482c5c49407305ddd99e2f583a9f
borin272 <aquieopatrickkrl@gmail.com>
2026-01-26 18:36:45 -0300

    Layout mobile imcompleto, commit feito para salvar e trocar de PC

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
     apiKey: "AIzaSyCfoxtQy_MCf5_fqKFq4OsXYvvyhkmwEYo",
@@ -15,6 +16,7 @@ const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const storage = getStorage(app);
 
 // Função auxiliar para criar/modificar usuário
 export const createOrUpdateUser = async (userId, userData) => {
